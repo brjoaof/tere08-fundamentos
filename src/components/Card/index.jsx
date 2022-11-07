5c3c26ea1b284739252e0720ab655415fa5ce6e0
Joao Felipe Braganca <joao.felipe@venturus.org.br>
2022-11-07 19:55:44 -0300

    add DELETE and UPDATE

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,14 @@
 import { Card, Button } from 'react-bootstrap';
 
-const CardTask = ({ task }) => {
+const CardTask = ({ task, deleteTask, setDescricao, setPrioridade, setTitulo, setEditing }) => {
+
+    const handleUpdate = () => {
+        console.log(task)
+        setDescricao(task.descricao)
+        setPrioridade(task.prioridade)
+        setTitulo(task.titulo)
+        setEditing({id: task.id, edit: true})
+    }
     return (
         <Card className="mb-2">
             <Card.Header as="h5">{task.titulo}</Card.Header>
@@ -8,6 +16,8 @@ const CardTask = ({ task }) => {
                 <Card.Text>
                 {task.descricao}
                 </Card.Text>
+            <Button onClick={() => deleteTask(task.id)}>Excluir</Button>
+            <Button className="ms-3" onClick={handleUpdate}>Editar</Button>
             </Card.Body>
         </Card>
     )
